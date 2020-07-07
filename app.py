820f070d5c536836c2d6098d11d15f871b1770be
Roqvie <vmandrch@gmail.com>
2020-07-07 18:36:01 +0300

    Added primitive polynomial renderer

--- app.py
+++ app.py
@@ -1,38 +1,66 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import (QtWidgets, QtSvg)
+from PyQt5.QtGui import (QIcon, QPixmap, QPainter)
 from PyQt5.QtCore import QSize
 from design import MainWindowUI
-from utils import generate_button
+from utils import (generate_button, _render)
 
 
 class CalculatorUI(QtWidgets.QMainWindow, MainWindowUI):
     # Constants for field
     P, N = 2, 3
-    PR_BUTTONS = []
+    calc_buffer = []
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.p.editingFinished.connect(self.set_input_buttons)
-        self.n.editingFinished.connect(self.set_input_buttons)
+        # Generate buttons for standart (p,n) parameters
+        self.generate_buttons(self.PrimitiveElementsGrid)
+        self.p.editingFinished.connect(lambda: self.generate_buttons(self.PrimitiveElementsGrid))
+        self.n.editingFinished.connect(lambda: self.generate_buttons(self.PrimitiveElementsGrid))
+        self.append.clicked.connect(self._plus)
 
 
-    def set_input_buttons(self):
-        """Generate buttons for field with (p,n) parameters"""
+    def generate_buttons(self, grid):
+        """Generate buttons for grid with (p,n) parameters"""
 
-        # Delete all buttons from layout
-        if self.PrElementsGrid.count():
-            for i in range(self.PrElementsGrid.count()):
-                self.PrElementsGrid.itemAt(i).widget().close()
+        # Delete all buttons from grid
+        if grid.count():
+            for i in range(grid.count()):
+                grid.itemAt(i).widget().close()
 
-        # Set parameters
+        # Check parameters
         given_p, given_n = int(self.p.text()), int(self.n.text())
         p = given_p if given_p else self.P
         n = given_n if given_n else self.N
 
-        self._create_grid(p, n, 8, self.PrElementsGrid)
+        # Generate buttons
+        self.create_grid(p, n, 8, grid)
 
 
-    def _create_grid(self, p, n, cols, grid):
+    def create_grid(self, p, n, cols, grid):
+        """Put generated buttons in grid"""
+
         for i in range(n+1):
-            grid.addWidget(generate_button(i, 'x'), i//cols, i%cols, 1, 1)
+            # Generate button with TeX string: 'x^{i}'
+            button = generate_button(i, 'x')
+            grid.addWidget(button, i//cols, i%cols, 1, 1)
+            button.clicked.connect(self._add)
+
+
+    def _add(self):
+        sender = self.sender()
+        if len(self.calc_buffer):
+            self.calc_buffer.append([self.calc_buffer[-1][0] + sender.tex, sender.power])
+        else:
+            self.calc_buffer.append([sender.tex, sender.power])
+
+        pixmap = QPixmap()
+        pixmap.loadFromData(_render(self.calc_buffer[-1][0]))
+        self.lable.setPixmap(pixmap)
+
+    def _plus(self):
+        if len(self.calc_buffer):
+            self.calc_buffer.append([self.calc_buffer[-1][0] + "+", None])
+            pixmap = QPixmap()
+            pixmap.loadFromData(_render(self.calc_buffer[-1][0]))
+            self.lable.setPixmap(pixmap)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import (QtWidgets, QtSvg)
+from PyQt5.QtGui import (QIcon, QPixmap, QPainter)
 from PyQt5.QtCore import QSize
 from io import BytesIO
 import matplotlib.pyplot as plot
@@ -11,31 +11,45 @@ class CustomPushButton(QtWidgets.QPushButton):
         super().__init__()
         self.power = None
         self.base = None
+        self.tex = None
 
 
 def generate_button(power, base):
     """Generate button with TeX svg-icon"""
 
     pixmap = QPixmap()
-    pixmap.loadFromData(_render(power, base))
+    pixmap.loadFromData(_render(tex_string=get_tex_string(power, base)))
     pushButton = CustomPushButton()
     pushButton.setIcon(QIcon(pixmap))
-    pushButton.setIconSize(QSize(40,28))
+    pushButton.setIconSize(QSize(40,25))
     pushButton.power = power
     pushButton.base = base
+    pushButton.tex = get_tex_string(power, base)
     return pushButton
 
+def get_tex_string(power, base):
+    """Generate TeX-based string from parameters"""
+    if power == 0:
+        return r'$1$'
+    elif power == 1:
+        return r'$%s$' % base
+    else:
+        return r'$%s^{%s}$' % (base, power)
 
-def _render(power, base, format='svg', fontsize=16):
-    """Render TeX string to svg"""
 
-    tex_string = r'$%s^{%s}$' % (base,power)
+def _render(tex_string, filename=None, format='png', fontsize=16, bytes=True):
+    """Render TeX string to svg-file"""
+
     plot.rc('mathtext', fontset='cm')
     figure = plot.figure(figsize=(0.01, 0.01))
     figure.text(0, 0, tex_string, fontsize=fontsize, color='black')
     # Save rendered svg in bytes
-    output = BytesIO()
-    figure.savefig(output, format=format, dpi=300, transparent=True, bbox_inches='tight', pad_inches=0.02)
-    plot.close(fig=figure)
-    output.seek(0)
-    return output.read()
+    if bytes:
+        output = BytesIO()
+        figure.savefig(output, format=format, dpi=200, transparent=True, bbox_inches='tight', pad_inches=0.02)
+        plot.close(fig=figure)
+        output.seek(0)
+        return output.read()
+    else:
+        figure.savefig(filename, format=format, transparent=True, bbox_inches='tight', pad_inches=0.02)
+        plot.close(fig=figure)
